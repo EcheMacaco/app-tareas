dc00f6555efd0a9a12ffe3fe8a8d80a2ed6aab35
Eche <eche_@hotmail.com>
2022-08-03 14:22:58 -0300

    app repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import ListaDeTareas from "./componentes/ListaDeTareas";
+import TareaFormulario from "./componentes/TareaFormulario";
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className="tareas-lista-principal">
+        <h1> Mis tareas</h1>
+        <ListaDeTareas />
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
